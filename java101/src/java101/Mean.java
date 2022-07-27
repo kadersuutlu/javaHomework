e59a2d6e192ae3011a5b965b766f9d53fbb8dcab
Kader Sutlu <80063396+kadersuutlu@users.noreply.github.com>
2022-07-27 23:34:04 +0300

    Not Ortalaması Hesaplayan Program

---
+++ java101/src/java101/Mean.java
@@ -0,0 +1,43 @@
+package java101;
+
+import java.util.Scanner;
+
+public class Mean {
+
+	public static void main(String[] args) {
+		
+		int mat,fizik,kimya,biyoloji,edebiyat,müzik;
+		
+		int mean;
+		
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Mat notu: ");
+		mat=scan.nextInt();
+		System.out.println("Fizik notu: ");
+		fizik=scan.nextInt();
+		System.out.println("Kimya notu: ");
+		kimya=scan.nextInt();
+		System.out.println("Biyoloji notu: ");
+		biyoloji=scan.nextInt();
+		System.out.println("Edebiyat notu");
+		edebiyat=scan.nextInt();
+		System.out.println("Muzik notu: ");
+		müzik=scan.nextInt();
+		
+		mean=(mat+fizik+kimya+biyoloji+edebiyat+müzik)/6;
+		
+		System.out.println("Ortalama: " + mean + "\n" );
+		System.out.println(mean>=60 ? "Sinifi Gecti" : "Sinifta Kaldi");
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
